83058b1b989edf203b3eb4f273df0fddb1715acb
HX Lin <>
2026-06-24 16:23:00 +0800

    Remove trailing whitespace

--- src/common/log_monitor.types.h
+++ src/common/log_monitor.types.h
@@ -6,9 +6,9 @@
 #include "log_monitor_types.h"
 
 
-namespace dsn { namespace tools { 
+namespace dsn { namespace tools {
     GENERATED_TYPE_SERIALIZATION(log_monitor_entry, THRIFT)
 
     DEFINE_TASK_CODE_RPC(RPC_LOG_MONITOR_REPORT, TASK_PRIORITY_HIGH, THREAD_POOL_DEFAULT)
 
-} } 
+} }
